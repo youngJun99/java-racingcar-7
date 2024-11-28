ac1e76b73e9e0961021c969ad758df791d7ce492
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-28 19:59:53 +0900

    feat(Errors): 에러 구현 및 입력 관련 에러 생성

---
+++ src/main/java/racingcar/constants/Errors.java
@@ -0,0 +1,21 @@
+package racingcar.constants;
+
+public enum Errors {
+
+    //입력 에러
+    INVALID_CAR_NAMES_INPUT("자동차 이름 입력 형식이 잘못되었습니다"),
+    INVALID_TOKENS_INPUT("게임 횟수 입력 형식이 잘못되었습니다.");
+
+
+    private static final String PREFIX = "[ERROR] ";
+
+    private String message;
+
+    Errors(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return PREFIX + message;
+    }
+}
