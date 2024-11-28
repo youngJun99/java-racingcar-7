91c1ec4c2f5022a82b20a1321d9142797603ad19
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-28 20:02:02 +0900

    feat(InputValidator): 구현 완료

---
+++ src/main/java/racingcar/handler/InputValidator.java
@@ -0,0 +1,21 @@
+package racingcar.handler;
+
+import racingcar.constants.Errors;
+
+public class InputValidator {
+
+    private static final String CAR_NAME_VALIDATION_REGEX = "^([가-힣a-zA-Z]+)(,[가-힣a-zA-Z]+)*$ ";
+    private static final String TOKEN_VALIDATION_REGEX = "\\d+";
+
+    public void validateCarNamesInput(String input) {
+        if (!input.matches(CAR_NAME_VALIDATION_REGEX)) {
+            throw new IllegalArgumentException(Errors.INVALID_CAR_NAMES_INPUT.getMessage());
+        }
+    }
+
+    public void validateGameTokensInput(String input) {
+        if (!input.matches(TOKEN_VALIDATION_REGEX)) {
+            throw new IllegalArgumentException(Errors.INVALID_TOKENS_INPUT.getMessage());
+        }
+    }
+}
