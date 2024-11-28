3c2d7474d3522b99c641d97035d60ff56a01350a
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-28 20:53:00 +0900

    feat(Controller): 구현 완료

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -1,7 +1,11 @@
 package racingcar.controller;
 
+import racingcar.dto.RoundResultDto;
 import racingcar.service.RaceService;
 import racingcar.view.OutputView;
+
+import java.util.List;
+
 
 public class Controller {
 
@@ -11,6 +15,13 @@ public class Controller {
     public Controller(RaceService raceService, OutputView outputView) {
         this.raceService = raceService;
         this.outputView = outputView;
+    }
+
+    public void run() {
+        List<RoundResultDto> results = raceService.getRoundResult();
+        List<String> winners = raceService.getWinners();
+        outputView.printRoundResults(results);
+        outputView.printWinners(winners);
     }
 
 }
